c8d7ad4822528517b4c37bf33f27e7a6c3793b25
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-04-19 17:28:21 -0400

    Update audio-player.js

--- tools/audio-player.js
+++ tools/audio-player.js
@@ -30,8 +30,8 @@ function addConnections(audioId) {
 
 addButton.onclick = function() {
 	const clone = audioPlaceholder.cloneNode(true)
-	clone.querySelector("#audio_file" + audio_number).id = "audio_file" + String(audio_number + 1)
-	clone.querySelector("#audio_url" + audio_number).id = "audio_url" + String(audio_number + 1)
+	clone.querySelector("#audio_file1").id = "audio_file" + String(audio_number + 1)
+	clone.querySelector("#audio_url1").id = "audio_url" + String(audio_number + 1)
 	clone.id = "audio" + String(audio_number + 1)
 	document.body.appendChild(clone)
 	audio_number += 1
