221d62eb8599d72dc98c1a1db86130577cb183cf
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-04-19 16:51:49 -0400

    Update audio-player.js

--- tools/audio-player.js
+++ tools/audio-player.js
@@ -1,3 +1,11 @@
+const audioPlaceholder = document.getElementById("audio1").cloneNode(true)
+const addButton = document.createElement("button")
+
+let audio_number = 1
+
+addButton.innerText = "+"
+document.body.appendChild(addButton)
+
 function addConnections(audioId) {
 	const url = document.getElementById("audio_url" + audioId)
 	const fileInput = document.getElementById("audio_file" + audioId)
@@ -18,6 +26,18 @@ function addConnections(audioId) {
 			url.disabled = true
 		}
 	})
+}
+
+addButton.onclick = function() {
+	const clone = audioPlaceholder.cloneNode(true)
+	clone.getElementById("audio_file" + audio_number).id = "audio_file" + String(audio_number + 1)
+	clone.getElementById("audio_url" + audio_number).id = "audio_url" + String(audio_number + 1)
+	clone.id = "audio" + String(audio_number + 1)
+	document.body.appendChild(clone)
+	audio_number += 1
+	addButton.remove()
+	document.body.appendChild(addButton)
+	addConnections(String(audio_number))
 }
 
 addConnections("1")
